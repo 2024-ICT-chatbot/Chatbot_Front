e3f64cc2056409c22e6943cdbc29b5bcd70e58bd
Castle-Jun <sinsung1084@naver.com>
2024-08-11 16:01:22 +0900

    UI 변경 1
    UI 1

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useRef } from 'react';
 import './ChatMessage.css';
 import botCharacter from '../assets/images/bot_character.png';
 
-
 const ChatMessage = ({ message, handleButtonClick }) => {
     const isUser = message.type === 'user';
     const chatEndRef = useRef(null);
@@ -12,51 +11,57 @@ const ChatMessage = ({ message, handleButtonClick }) => {
         chatEndRef.current?.scrollIntoView({ behavior: 'smooth' });
     }, [message]);
 
-    if (message.isWelcome) {
-        return (
-            <div>
-                <div className="bot-header">
-                    {/* 챗봇 이미지와 이름 */}
-                    <img src={botCharacter} alt="bot" className="bot-image" />
-                    <div className="bot-name">항만공사 챗봇</div>
-                </div>
-                <div className="welcome-box">
-                    {/* 안내 메시지 */}
-                    <div>안녕하세요? 항만공사 챗봇입니다!</div>
-                    <div className="spacing" /> 
-                    <div>무엇을 도와드릴까요?</div>
-                    <div className="spacing" /> 
-                    <div>키워드 입력보다는 대화형으로 문의하시면 더 잘 알려드릴 수 있어요!</div>
-                    {/* 메시지와 버튼 사이의 간격을 위한 div 추가 */}
-                    <div className="spacing" /> 
-                    <div className="buttons">
-                        {/* 버튼들 */}
-                        <button onClick={() => handleButtonClick('facility')}>시설 예약</button>
-                        <button onClick={() => handleButtonClick('cargo')}>화물 입·출항 신고</button>
-                        <button onClick={() => handleButtonClick('repair')}>수리 요청</button>
-                        <button onClick={() => handleButtonClick('payment')}>요금 결제</button>
-                        <button onClick={() => handleButtonClick('consult')}>상담 접수</button>
-                    </div>
-                </div>
-                <div ref={chatEndRef} />
-            </div>
-        );
-    }
-
     return (
-        <div className={`chat-message ${isUser ? 'user' : 'bot'}`}>
-            {!isUser && (
-                <div className="bot-info">
-                    {/* 챗봇 이미지 */}
-                    <img src={botCharacter} alt="bot" className="bot-image" /> 
+        <div className="chat-message-container">
+            {message.isWelcome ? (
+                <div className={`chat-message bot`}>
+                    <div className="bot-info">
+                        <img src={botCharacter} alt="bot" className="bot-image" />
+                    </div>
+                    <div className="message-container bot">
+                        <div className="message-bubble">
+                            <p>안녕하세요? 항만공사 챗봇입니다!</p>
+                            <p>무엇을 도와드릴까요?</p>
+                            <p>키워드 입력보다는 대화형으로 문의하시면 더 잘 알려드릴 수 있어요!</p>
+                        </div>
+                    </div>
+                    <div className="buttons-outside">
+                        <div className="buttons-row">
+                            <button onClick={() => handleButtonClick('facility')} className="wide-button">시설 예약</button>
+                            <button onClick={() => handleButtonClick('cargo')} className="wide-button">화물 입·출항 신고</button>
+                        </div>
+                        <div className="buttons-row">
+                            <button onClick={() => handleButtonClick('repair')}>수리 요청</button>
+                            <button onClick={() => handleButtonClick('payment')}>요금 결제</button>
+                            <button onClick={() => handleButtonClick('consult')}>고객 지원</button>
+                        </div>
+                    </div>
+                    <div ref={chatEndRef} />
+                </div>
+            ) : (
+                <div className={`chat-message ${isUser ? 'user' : 'bot'}`}>
+                    {!isUser && (
+                        <div className="bot-info">
+                            <img src={botCharacter} alt="bot" className="bot-image" />
+                        </div>
+                    )}
+                    <div className={`message-container ${isUser ? 'user' : 'bot'}`}>
+                        {!isUser && (
+                            <>
+                                <div className="message-bubble">{message.text}</div>
+                                <div className="timestamp bot-timestamp">{message.timestamp}</div>
+                            </>
+                        )}
+                        {isUser && (
+                            <>
+                                <div className="timestamp user-timestamp">{message.timestamp}</div>
+                                <div className="message-bubble">{message.text}</div>
+                            </>
+                        )}
+                    </div>
+                    <div ref={chatEndRef} />
                 </div>
             )}
-            <div className={`message-bubble ${isUser ? 'user' : 'bot'}`}>
-                {Array.isArray(message.text) ? message.text.map((line, index) => (
-                    <p key={index}>{line}</p>
-                )) : <p>{message.text}</p>}
-            </div>
-            <div ref={chatEndRef} />
         </div>
     );
 };
