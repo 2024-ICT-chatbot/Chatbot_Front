e4cf8d61af576556370de9fa41ef3d3003944e7e
조성빈 <whtjdqlsqp@joseongbin-ui-MacBookAir.local>
2024-10-06 18:58:51 +0900

    fix: 최종수정

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -40,208 +40,114 @@ const convertTextToLinks = (text) => {
   
   
 
-const ChatMessage = ({ message, handleButtonClick }) => {
-  const isUser = message.type === "user";
-  const chatEndRef = useRef(null);
 
-  // 새로운 메시지가 추가될 때마다 스크롤을 최하단으로 이동
-  useEffect(() => {
-    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [message]);
+  const ChatMessage = ({ message, handleButtonClick }) => {
+    const isUser = message.type === 'user';
+    const chatEndRef = useRef(null);
 
+    // 새로운 메시지가 추가될 때마다 스크롤을 최하단으로 이동
+    useEffect(() => {
+        chatEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [message]);
 
-  return (
-    <div className="chat-message-container">
-      {message.isWelcome ? (
-        <div className={`chat-message bot`}>
-          <div className="bot-info">
-            <img src={botCharacter} alt="bot" className="bot-image" />
-          </div>
-          <div className="message-container bot">
-            <div className="message-bubble">
-              <p>안녕하세요? 항만공사 챗봇입니다!</p>
-              <p>무엇을 도와드릴까요?</p>
-              <p>
-                키워드 입력보다는 대화형으로 문의하시면 더 잘 알려드릴 수
-                있어요!
-              </p>
-            </div>
-            <div className="timestamp bot-timestamp">{message.timestamp}</div>
-          </div>
-          <div className="buttons-outside">
-            <div className="buttons-row">
-              <button
-                onClick={() => handleButtonClick("cargo")}
-                className="wide-button"
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    src={harborImage}
-                    alt="항만"
-                    style={{
-                      width: "40px",
-                      height: "40px",
-                      marginBottom: "8px",
-                    }}
-                  />
-                  <div>항만 입/출항 신고</div>
-
-                </div>
-              </button>
-              <button
-                onClick={() => handleButtonClick("payment")}
-                className="wide-button"
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    src={fee}
-                    alt="요금결제"
-                    style={{
-                      width: "40px",
-                      height: "40px",
-                      marginBottom: "8px",
-                    }}
-                  />
-                  <div>항만시설사용료</div>
-                </div>
-              </button>
-              <button
-                onClick={() => handleButtonClick("support")}
-                className="wide-button"
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    src={support}
-                    alt="고객지원"
-                    style={{
-                      width: "40px",
-                      height: "40px",
-                      marginBottom: "8px",
-                    }}
-                  />
-                  <div>고객 지원</div>
-                </div>
-              </button>
-            </div>
-            <div className="buttons-row">
-              <button
-                onClick={() => handleButtonClick("info")}
-                className="wide-button"
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    src={imformation}
-                    alt="기타정보"
-                    style={{
-                      width: "40px",
-                      height: "40px",
-                      marginBottom: "8px",
-                    }}
-                  />
-                  <div>기타 정보</div>
-                </div>
-              </button>
-              <button
-                onClick={() => handleButtonClick("service_intro")}
-                className="wide-button"
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                  }}
-                >
-                  <img
-                    src={serviceIntro}
-                    alt="서비스 소개"
-                    style={{
-                      width: "40px",
-                      height: "40px",
-                      marginBottom: "8px",
-                    }}
-                  />
-                  <div>서비스 소개</div>
-                </div>
-              </button>
-            </div>
-          </div>
-          <div ref={chatEndRef} />
-        </div>
-      ) : (
-        <div className={`chat-message ${isUser ? "user" : "bot"}`}>
-          {!isUser && (
-            <>
-              <div className="bot-info">
-                <img src={botCharacter} alt="bot" className="bot-image" />
-              </div>
-              <div className="message-container bot">
-                <div className="message-bubble">
-                  {Array.isArray(message.text)
-                    ? message.text.map((line, index) => (
-                        <p key={index}>{convertTextToLinks(line)}</p>
-                      ))
-                    : convertTextToLinks(message.text)}
-                </div>
-                <div className="timestamp bot-timestamp">
-                  {message.timestamp}
-                </div>
-              </div>
-              {message.options && (
-                <div className="additional-options">
-                  {message.options.map((option, idx) => (
-                    <div key={idx} className="option-wrapper">
-                      <button
-                        onClick={() => handleButtonClick(option)}
-                        className="option-button"
-                      >
-                        {option}
-                      </button>
+    return (
+        <div className="chat-message-container">
+            {message.isWelcome ? (
+                <div className={`chat-message bot`}>
+                    <div className="bot-info">
+                        <img src={botCharacter} alt="bot" className="bot-image" />
                     </div>
-                  ))}
+                    <div className="message-container bot">
+                        <div className="message-bubble">
+                            <p>안녕하세요? 항만공사 챗봇입니다!</p>
+                            <p>무엇을 도와드릴까요?</p>
+                            <p>키워드 입력보다는 대화형으로 문의하시면 더 잘 알려드릴 수 있어요!</p>
+                        </div>
+                        <div className="timestamp bot-timestamp">{message.timestamp}</div>
+                    </div>
+                    <div className="buttons-outside">
+                        <div className="buttons-row">
+                            <button onClick={() => handleButtonClick('cargo')} className="wide-button">
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                    <img src={harborImage} alt="항만" style={{ width: '40px', height: '40px', marginBottom: '8px' }} />
+                                    <div>항만 입/출항 신고</div>
+                                </div>
+                            </button>
+                            <button onClick={() => handleButtonClick('payment')} className="wide-button">
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                    <img src={fee} alt="요금결제" style={{ width: '40px', height: '40px', marginBottom: '8px' }} />
+                                    <div>항만시설사용료</div>
+                                </div>
+                            </button>
+                            <button onClick={() => handleButtonClick('support')} className="wide-button">
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                    <img src={support} alt="고객지원" style={{ width: '40px', height: '40px', marginBottom: '8px' }} />
+                                    <div>고객 지원</div>
+                                </div>
+                            </button>
+                        </div>
+                        <div className="buttons-row">
+                            <button onClick={() => handleButtonClick('info')} className="wide-button">
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                    <img src={imformation} alt="기타정보" style={{ width: '40px', height: '40px', marginBottom: '8px' }} />
+                                    <div>기타 정보</div>
+                                </div>
+                            </button>
+                            <button onClick={() => handleButtonClick('service_intro')} className="wide-button">
+                                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                    <img src={serviceIntro} alt="서비스 소개" style={{ width: '40px', height: '40px', marginBottom: '8px' }} />
+                                    <div>서비스 소개</div>
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                    <div ref={chatEndRef} />
                 </div>
-              )}
-            </>
-          )}
-          {isUser && (
-            <div className="message-container user">
-              <div className="message-bubble">
-                {convertTextToLinks(message.text)}
-              </div>
-              <div className="timestamp user-timestamp">
-                {message.timestamp}
-              </div>
-            </div>
-          )}
+            ) : (
+                <div className={`chat-message ${isUser ? 'user' : 'bot'}`}>
+                    {!isUser && (
+                        <>
+                            <div className="bot-info">
+                                <img src={botCharacter} alt="bot" className="bot-image" />
+                            </div>
+                            <div className="message-container bot">
+                                <div className="message-bubble">
+                                    {message.isIframe ? (
+                                        <div dangerouslySetInnerHTML={{ __html: message.text }} />
+                                    ) : (
+                                        Array.isArray(message.text) ? (
+                                            message.text.map((line, index) => <p key={index}>{convertTextToLinks(line)}</p>)
+                                        ) : (
+                                            convertTextToLinks(message.text)
+                                        )
+                                    )}
+                                </div>
+                                <div className="timestamp bot-timestamp">{message.timestamp}</div>
+                            </div>
+                            {message.options && (
+                                <div className="additional-options">
+                                    {message.options.map((option, idx) => (
+                                        <div key={idx} className="option-wrapper">
+                                            <button onClick={() => handleButtonClick(option)} className="option-button">
+                                                {option}
+                                            </button>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                        </>
+                    )}
+                    {isUser && (
+                        <div className="message-container user">
+                            <div className="message-bubble">{convertTextToLinks(message.text)}</div>
+                            <div className="timestamp user-timestamp">{message.timestamp}</div>
+                        </div>
+                    )}
+                </div>
+            )}
+            <div ref={chatEndRef} />
         </div>
-      )}
-      <div ref={chatEndRef} />
-    </div>
-  );
+    );
 };
 
 export default ChatMessage;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import ChatMessage from '../components/ChatMessage';
-import '../containers/App.css';
-import sendimage from '../assets/images/전송.png';
-import DynamicForm from '../containers/DynamicForm';
-import axios from 'axios';
+  import ChatMessage from '../components/ChatMessage';
+  import '../containers/App.css';
+  import sendimage from '../assets/images/전송.png';
+  import DynamicForm from '../containers/DynamicForm';
+  import axios from 'axios';
 
+  const App = () => {
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState('');
 
-const App = () => {
-  const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState("");
+    useEffect(() => {
+      const welcomeMessage = {
+        type: 'bot',
+        text: ['안녕하세요? 항만공사 챗봇입니다.', '무엇을 도와드릴까요?'],
+        isWelcome: true,
+        timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+      };
+      setMessages([welcomeMessage]);
+    }, []);
 
-  useEffect(() => {
-    const welcomeMessage = {
-      type: "bot",
-      text: ["안녕하세요? 항만공사 챗봇입니다.", "무엇을 도와드릴까요?"],
-      isWelcome: true,
-      timestamp: new Date().toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      }),
-    };
-    setMessages([welcomeMessage]);
-  }, []);
-
-  const handleButtonClick = async (infoType) => {
-    let newMessages = [...messages];
+    const handleButtonClick = async (infoType) => {
+      let newMessages = [...messages];
 
       // 정해진 옵션 리스트에 따라 API 호출
       if (infoType === '방문 예약') {
@@ -49,150 +44,96 @@ const App = () => {
             params: {
               button_name: infoType,
             },
-          }
-        );
-        const botResponse = response.data;
+          });
+          const botResponse = response.data;
 
-        newMessages = [
-          ...messages,
-          {
-            type: "bot",
-            text: botResponse.message,
-            timestamp: new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-            }),
-            options: botResponse.options || [],
-          },
-        ];
-      } catch (error) {
-        console.error("Error fetching data from backend:", error);
+          newMessages = [
+            ...messages,
+            {
+              type: 'bot',
+              text: botResponse.message,
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+              options: botResponse.options || [],
+            },
+          ];
+        } catch (error) {
+          console.error('Error fetching data from backend:', error);
 
-        newMessages = [
-          ...messages,
-          {
-            type: "bot",
-            text: "오류가 발생했습니다. 다시 시도해주세요.",
-            timestamp: new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-            }),
-          },
-        ];
+          newMessages = [
+            ...messages,
+            {
+              type: 'bot',
+              text: '오류가 발생했습니다. 다시 시도해주세요.',
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+            },
+          ];
+        }
+      } else {
+        // 기본적인 옵션을 처리하는 부분
+        if (infoType === 'cargo') {
+          newMessages = [
+            ...messages,
+            {
+              type: 'bot',
+              text: '항만 입출항 신고를 선택하셨습니다. 다음 작업을 선택해주세요.',
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+              options: ['외항선 입출항 수속 절차', '내항선 입출항 수속 절차', 'PORT-MIS 교육자료'],
+            },
+          ];
+        } else if (infoType === 'payment') {
+          newMessages = [
+            ...messages,
+            {
+              type: 'bot',
+              text: '항만시설사용료를 선택하셨습니다. 다음 작업을 선택해주세요.',
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+              options: ['항만시설사용료의 종류 및 징수대상시설', '사용료 기타정보'],
+            },
+          ];
+        } else if (infoType === '항만시설사용료의 종류 및 징수대상시설') {
+          newMessages = [
+            ...messages,
+            {
+              type: 'bot',
+              text: '항만시설사용료의 종류 및 징수대상시설에 대해 알아보겠습니다. 세부 항목을 선택해주세요.',
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+              options: ['선박료', '화물료', '항만시설 전용사용료', '항만시설 보안료', '임대료'],
+            },
+          ];
+        }
+        else if (infoType === 'support') {
+          newMessages = [
+            ...messages,
+            {
+              type: 'bot',
+              text: '고객 지원을 선택하셨습니다. 다음 작업을 선택해주세요.',
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+              options: ['울산항만공사 선석운영지원시스템', '울산항 데이터통합플랫폼 PortWise', '방문 예약'],
+            },
+          ];
+        } else if (infoType === 'service_intro') {
+          newMessages = [
+            ...messages,
+            {
+              type: 'bot',
+              text: '울산항만공사에서 제공하는 서비스 목록입니다. 다음 작업을 선택해주세요.',
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+              options: ['선박입·출항신고 수리', '선석운영 협의회 운영', '항만시설 사용실적 관리', '화물료 고지', '항만시설 사용신청 및 승낙'],
+            },
+          ];
+        }else if (infoType === 'info') {
+          newMessages = [
+            ...messages,
+            {
+              type: 'bot',
+              text: '기타 정보를 선택하셨습니다. 다음 작업을 선택해주세요.',
+              timestamp: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
+              options: ['채용 정보', '대국민 공모 신청'],
+            },
+          ];
+        }
       }
 
-    } else {
-      // 기본적인 옵션을 처리하는 부분
-      if (infoType === "cargo") {
-        newMessages = [
-          ...messages,
-          {
-            type: "bot",
-            text: "항만 입출항 신고를 선택하셨습니다. 다음 작업을 선택해주세요.",
-            timestamp: new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-            }),
-            options: [
-              "외항선 입출항 수속 절차",
-              "내항선 입출항 수속 절차",
-              "PORT-MIS 교육자료",
-            ],
-          },
-        ];
-      } else if (infoType === "payment") {
-        newMessages = [
-          ...messages,
-          {
-            type: "bot",
-            text: "항만시설사용료를 선택하셨습니다. 다음 작업을 선택해주세요.",
-            timestamp: new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-            }),
-            options: [
-              "항만시설사용료의 종류 및 징수대상시설",
-              "사용료 기타정보",
-            ],
-          },
-        ];
-      } else if (infoType === "항만시설사용료의 종류 및 징수대상시설") {
-        newMessages = [
-          ...messages,
-          {
-            type: "bot",
-            text: "항만시설사용료의 종류 및 징수대상시설에 대해 알아보겠습니다. 세부 항목을 선택해주세요.",
-            timestamp: new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-            }),
-            options: [
-              "선박료",
-              "화물료",
-              "항만시설 전용사용료",
-              "항만시설 보안료",
-              "임대료",
-            ],
-          },
-        ];
-      } else if (infoType === "support") {
-        newMessages = [
-          ...messages,
-          {
-            type: "bot",
-            text: "고객 지원을 선택하셨습니다. 다음 작업을 선택해주세요.",
-            timestamp: new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-            }),
-            options: [
-              "울산항만공사 선석운영지원시스템",
-              "울산항 데이터통합플랫폼 PortWise",
-              "방문 예약",
-            ],
-          },
-        ];
-      } else if (infoType === "service_intro") {
-        newMessages = [
-          ...messages,
-          {
-            type: "bot",
-            text: "울산항만공사에서 제공하는 서비스 목록입니다. 다음 작업을 선택해주세요.",
-            timestamp: new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-            }),
-            options: [
-              "선박입·출항신고 수리",
-              "선석운영 협의회 운영",
-              "항만시설 사용실적 관리",
-              "화물료 고지",
-              "항만시설 사용신청 및 승낙",
-            ],
-          },
-        ];
-      } else if (infoType === "info") {
-        newMessages = [
-          ...messages,
-          {
-            type: "bot",
-            text: "기타 정보를 선택하셨습니다. 다음 작업을 선택해주세요.",
-            timestamp: new Date().toLocaleTimeString([], {
-              hour: "2-digit",
-              minute: "2-digit",
-              hour12: false,
-            }),
-            options: ["채용 정보", "대국민 공모 신청"],
-          },
-        ];
-      }
       setMessages(newMessages);
     };
 
@@ -235,8 +176,9 @@ const App = () => {
           }
         }
       }
-    }
+    
 
+    };
 
     const handleKeyPress = (e) => {
       if (e.key === 'Enter') {
@@ -280,99 +222,4 @@ const App = () => {
     );
   };
 
-  const handleSend = async () => {
-    if (input.trim() !== "") {
-      const userMessage = {
-        type: "user",
-        text: input,
-        timestamp: new Date().toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-        }),
-      };
-      setMessages([...messages, userMessage]);
-      setInput("");
-
-      try {
-        const botResponse = await axios.post(
-          "http://localhost:8000/api/v1/chat",
-          { message: input }
-        );
-        const botMessage = {
-          type: "bot",
-          text: botResponse.data.answer, // response가 아닌 answer를 사용
-          timestamp: new Date().toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
-          }),
-        };
-        setMessages((prevMessages) => [...prevMessages, botMessage]);
-      } catch (error) {
-        console.error("Error fetching bot response:", error);
-        const errorMessage = {
-          type: "bot",
-          text: "오류가 발생했습니다. 다시 시도해주세요.",
-          timestamp: new Date().toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-            hour12: false,
-          }),
-        };
-        setMessages((prevMessages) => [...prevMessages, errorMessage]);
-      }
-    }
-  };
-
-  const handleKeyPress = (e) => {
-    if (e.key === "Enter") {
-      handleSend();
-    }
-  };
-
-  return (
-    <div className="chatbot-container">
-      <div className="chatbot">
-        <div className="header-text-container">
-          <p className="header-text">항만공사 챗봇</p>
-        </div>
-        <div className="content-container">
-          <div className="messages">
-            {messages.map((msg, index) => (
-              <ChatMessage
-                key={index}
-                message={msg}
-                handleButtonClick={handleButtonClick}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="input-area">
-          <input
-            type="text"
-            placeholder="내용을 입력하세요"
-            className="chat-input"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyPress={handleKeyPress}
-          />
-          <button onClick={handleSend} className="send-button">
-            <img
-              src={sendimage}
-              alt="전송"
-              style={{
-                width: "24px",
-                height: "22px",
-                position: "relative",
-                top: "-6px",
-              }}
-            />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+  export default App;
